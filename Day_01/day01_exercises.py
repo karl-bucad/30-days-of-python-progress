1e0dc8e3066ca8a6d12dc99ba4f863a0f826f9f5
Karl Bucad <bucadkarl@gmail.com>
2025-11-24 18:23:25 -0500

    Day 1: basic Python exercises

---
+++ Day_01/day01_exercises.py
@@ -0,0 +1,38 @@
+#Exercise Level 1
+#2
+print(3 + 4)   #addition
+print(3 - 4)   #subtraction
+print(3 * 4)   #multiplication
+print(3 % 4)   #modulus(remainder)
+print(3 / 4)   #division
+print(3 ** 4)  #exponential
+print(3 // 4)  #floor division operator(rounds down)
+
+#3
+print("Karl")
+print("Bucad")
+print("Philippines")
+print("I am enjoying 30 days of python")
+
+#4
+print(type(10)) #int
+print(type(9.8)) #float
+print(type(3.14)) #float
+print(type(4 - 4j)) #complex
+print(type(['Asbaneh', 'Python', 'Finland'])) #list
+print(type("Karl")) #string
+print(type("Bucad")) #string
+print(type("Philippines")) #string
+
+#Exercise Level 2
+#1
+
+#Exercise Level 3
+#1 - Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary
+print(6 - 4j) #Number
+print("Karl Bucad") #String
+print(3 == 3) #Boolean
+print([1, 2, 3 ,4 ,5]) #List
+print(('Michael Jordan', 'Lebron James', 'Kareem Abdul-Jabbar', 'Magic Johnson', 'Bill Russel')) #Tuple
+print({1, 2, 3, 4, 5}) #Set
+print({'First Name':'Karl'}) #Dictionary
---
+++ Day_01/day01_notes.md
@@ -0,0 +1,16 @@
+Use triple quotes(""") for multiline comments
+
+
+Operators I struggled on:
+- "%" operator is the modulus operator, which divides the numbers and outputs the remainders
+- "//" operator is the floor division operator, which rounds down to the nearest whole number after dividing
+
+
+To find datatype, use print(type())
+
+
+Datatypes I struggled on:
+- [ , , , ] = list (ordered collection which allows to store different data type items)
+- {x:y} = dictionary (unordered collection of data in a key value pair format)
+- ( , , , ) = tuple (ordered collection of different data types like list but tuples can not be modified once they are created. They are immutable)
+- { , , , } = set (collection of data types similar to list and tuple. Unlike list and tuple, set is not an ordered collection of items. Like in Mathematics, set in Python stores only unique items)
